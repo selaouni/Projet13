3c26f407c6cacff58ec506f3974f628a394782bc
sabah <sabah.elaouni@gmail.com>
2023-02-11 21:52:04 +0100

    Update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,22 +120,22 @@ STATIC_URL = '/static/'
 
 
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-
-sentry_sdk.init(
-    dsn="https://fd8cb53f6e164c3f97f9e89751f9bc21@o4504618351788032.ingest.sentry.io/4504618373611520",
-    integrations=[
-        DjangoIntegration(),
-    ],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
+#
+#
+# sentry_sdk.init(
+#     dsn="https://fd8cb53f6e164c3f97f9e89751f9bc21@o4504618351788032.ingest.sentry.io/4504618373611520",
+#     integrations=[
+#         DjangoIntegration(),
+#     ],
+#
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
+#
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
